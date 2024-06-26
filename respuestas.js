85edc4dd5e44dc1310d57d7fb351592efc27c9f1
gabandre <127366441+gabandre@users.noreply.github.com>
2024-06-25 20:21:10 -0600

    Update respuestas.js

--- respuestas.js
+++ respuestas.js
@@ -1,5 +1,5 @@
 function resultado() {
-    var p1, p2, p3, p4, p5, p6, p7, p8, nota;
+    var p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, nota;
 
     // 1a pregunta
     if (document.getElementById('p11').checked==true) {p1=1}
@@ -18,9 +18,33 @@ function resultado() {
     else {p5=0}
 
     //Categoria de Intervalos
+
+    if (document.getElementById('p66').checked==true) {p6=1}
+    else {p6=0}
+    // 6a pregunta
+    if (document.getElementById('p77').checked==true) {p7=1}
+    else {p7=0}
+    // 7a pregunta
+    if (document.getElementById('p88').checked==true) {p8=1}
+    else {p8=0}
+    // 8a pregunta
+    if (document.getElementById('p99').checked==true) {p9=1}
+    else {p9=0}
+    // 9a pregunta
+    if (document.getElementById('p1010').checked==true) {p10=1}
+    else {p10=0}
+
+//10a
+    if (document.getElementById('p1111').checked==true) {p11=1}
+    else {p11=0}
+    // 11a pregunta
+    if (document.getElementById('p1212').checked==true) {p12=1}
+    else {p12=0}
+
+    
     
 
-    nota=p1+p2+p3+p4+p5;
+    nota=p1+p2+p3+p4+p5+p6+p7+p8+p9+p10+p11+p12;
     
     
     return nota;
